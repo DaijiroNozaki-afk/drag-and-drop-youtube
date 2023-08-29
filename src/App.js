0eca970a05366b4fc213d6a743e4be57b08c749a
dnozaki <dnozaki88888888@gmail.com>
2023-08-29 15:11:17 +0900

    makeItems 20230829-01

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import './App.css';
 
+const items = [
+  {
+    id: 'item-0',
+    content: 'item 0',
+  },
+  {
+    id: 'item-1',
+    content: 'item 1',
+  },
+  {
+    id: 'item-2',
+    content: 'item 2',
+  },
+  {
+    id: 'item-3',
+    content: 'item 3',
+  },
+  {
+    id: 'item-4',
+    content: 'item 4',
+  },
+];
 function App() {
   return <div className="App">test</div>;
 }
